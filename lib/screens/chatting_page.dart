9cafab1a91fcf3c8a2451d92afbc39ee99e369c4
Hasan Alani <59187679+hasanalaniIT@users.noreply.github.com>
2023-01-12 03:58:27 +0300

    Added a Chatting screen to use as route later on for the project

---
+++ lib/screens/chatting_page.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:iau_chat/constants.dart';
+
+class ChattingScreen extends StatefulWidget {
+  const ChattingScreen({super.key});
+
+  @override
+  ChattingScreenState createState() => ChattingScreenState();
+}
+
+class ChattingScreenState extends State<ChattingScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: null,
+        actions: <Widget>[
+          IconButton(
+              icon: const Icon(Icons.close),
+              onPressed: () {
+                //Implement logout functionality
+              }),
+        ],
+        title: const Text('Chat'),
+        backgroundColor: Colors.lightBlueAccent,
+      ),
+      body: SafeArea(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Container(
+              decoration: myMessageContainerDecoration,
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  Expanded(
+                    child: TextField(
+                      onChanged: (value) {
+                        //Do something with the user input.
+                      },
+                      decoration: myMessageTextFieldDecoration,
+                    ),
+                  ),
+                  TextButton(
+                    onPressed: () {
+                      //Implement send functionality.
+                    },
+                    child: const Text(
+                      'Send',
+                      style: mySendButtonTextStyle,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
