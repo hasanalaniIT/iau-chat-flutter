1bc26e00d8d3d918da9527e5bbec03cc6dac9989
Hasan Alani <59187679+hasanalaniIT@users.noreply.github.com>
2023-01-14 05:33:15 +0300

    Adding Feature to send a text message and store it in firebase cloud database

--- lib/screens/chatting_page.dart
+++ lib/screens/chatting_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:iau_chat/helpers/style_utils.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class ChattingScreen extends StatefulWidget {
   const ChattingScreen({super.key});
@@ -12,17 +13,19 @@ class ChattingScreen extends StatefulWidget {
 
 class ChattingScreenState extends State<ChattingScreen> {
   final _fireBaseAuth = FirebaseAuth.instance;
+  final _fireStoreAuth = FirebaseFirestore.instance;
   late User userLogIn;
-
+  late String textMessage;
   @override
   void initState() {
     getUser();
     super.initState();
   }
+
   void getUser() async {
     try {
-      final currUser =  _fireBaseAuth.currentUser;
-      if (currUser != null){
+      final currUser = _fireBaseAuth.currentUser;
+      if (currUser != null) {
         userLogIn = currUser;
         print(userLogIn.email);
       }
@@ -30,6 +33,7 @@ class ChattingScreenState extends State<ChattingScreen> {
       print(e);
     }
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -39,8 +43,8 @@ class ChattingScreenState extends State<ChattingScreen> {
           IconButton(
               icon: const Icon(Icons.close),
               onPressed: () {
-               _fireBaseAuth.signOut();
-               Navigator.pop(context);
+                _fireBaseAuth.signOut();
+                Navigator.pop(context);
               }),
         ],
         title: const Text('Chat'),
@@ -58,15 +62,18 @@ class ChattingScreenState extends State<ChattingScreen> {
                 children: <Widget>[
                   Expanded(
                     child: TextField(
-                      onChanged: (value) {
-                        //Do something with the user input.
+                      onChanged: (text) {
+                        textMessage = text;
                       },
                       decoration: myMessageTextFieldDecoration,
                     ),
                   ),
                   TextButton(
                     onPressed: () {
-                      //Implement send functionality.
+                      _fireStoreAuth.collection('conversations').add({
+                        "sent_message": textMessage,
+                        "user_mail": userLogIn.email,
+                      });
                     },
                     child: const Text(
                       'Send',
