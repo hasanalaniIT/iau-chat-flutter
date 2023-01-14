170ffeb9008e377ffcc5ac4565d8f269ca2df12b
Hasan Alani <59187679+hasanalaniIT@users.noreply.github.com>
2023-01-14 04:27:25 +0300

    Added firebase initializer when running the application

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,15 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:iau_chat/screens/main_page.dart';
 import 'package:iau_chat/screens/login_page.dart';
 import 'package:iau_chat/screens/signup_page.dart';
 import 'package:iau_chat/screens/chatting_page.dart';
 
-void main() => runApp(const MyChat());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(const MyChat());
+}
 
 class MyChat extends StatelessWidget {
   const MyChat({super.key});
